735012defe70642eab17c7b07a248c31243849d9
Daniel Knoll <knollmdan@gmail.com>
2024-01-20 15:56:27 -0500

    added foundational files and folders, installed fatfree, and set up a default route in index.php

---
+++ index.php
@@ -0,0 +1,23 @@
+<?php
+
+// This is my controller
+
+ini_set('display_errors', 1);
+error_reporting(E_ALL);
+
+//start a session
+session_start();
+// Require autoload file
+require_once('vendor/autoload.php');
+
+// Instantiate F3 Base Class
+$f3 = Base::instance();
+
+// Define a default route (328/application)
+$f3->route('GET /', function() {
+    $view = new Template();
+    echo $view->render('views/home.html');
+});
+
+// Run Fat Free
+$f3->run();
